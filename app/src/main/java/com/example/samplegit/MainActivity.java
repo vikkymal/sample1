520eaa3f33dc374871baf2b24a5ab5296eb4470e
vivek <viveknayak33@gmail.com>
2021-01-14 22:19:29 +0530

    Third commit

--- app/src/main/java/com/example/samplegit/MainActivity.java
+++ app/src/main/java/com/example/samplegit/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //second commit
+        //Third commit
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
